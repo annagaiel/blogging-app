b855cd4c0280a48ecf2241c2a5f5c20d1f6809c6
Anna Natorilla <annagaiel@gmail.com>
2016-06-09 19:46:25 -0700

    Add Crud with strong params

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,48 @@
 class PostsController < ApplicationController
 
   def index
+    @posts = Post.all
   end
 
   def show
+    @post = Post.find_by(id: params[:id])
   end
 
   def new
-    @post = Post.new()
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to edit_post_path(@post), notice: "Updated #{@post.title}"
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @post = Post.find_by(id: params[:id])
+    @post.destroy
+    redirect_to '/'
+  end
+
+  def edit
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def update
+    @post = Post.find_by(id: params[:id])
+    if @post.update(post_params)
+      redirect_to post_path, notice: "Updated #{@post.title}"
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
   end
 end
